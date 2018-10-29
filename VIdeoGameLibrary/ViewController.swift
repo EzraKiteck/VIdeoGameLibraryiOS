0f7380a0446d39787099f7799fd0576cca96dcf2
Ezra Kiteck <ezrakiteck@Interapts-MacBook-Pro-46.local>
2018-10-29 10:23:58 -0500

    Initial Commit

---
+++ VIdeoGameLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIdeoGameLibrary
+//
+//  Created by Ezra Kiteck on 10/29/18.
+//  Copyright © 2018 Ezra Kiteck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
